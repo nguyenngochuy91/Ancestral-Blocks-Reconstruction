4fd2b5eae0f3119d48cc0b5448a5c55337715d79
Huy Nguyen <huyn@iastate.edu>
2016-05-10 14:31:26 -0500

    adding color, and check
    manually do the colo for E.Coli, will soon update for any
    adding coloring for edge

--- show_tree.py
+++ show_tree.py
@@ -6,7 +6,7 @@
     End     : 05/08/2016
 '''
 from ete3 import *
-
+import os
 # color group of genome
 # create a dictionary, sadly this is done manually
 alpha=('Cau','Bra','Sin','Agr','Mes','Bru')
@@ -22,29 +22,35 @@ color_dic[delta]='brown'
 
 
 def main():
-    f= input("Which operon you want to show the tree:")
-    tree= Tree('./reconstruction/'+f)
+    f= raw_input("Which operon you want to show the tree:")
+    bacteria = raw_input("Which bacteria you want to show the tree (E.Coli or B.Sub):")
+    # print os.getcwd()
+    string = './reconstruction_'+bacteria+ '/'+f
+    tree= Tree(string)
     # using the color dic to color group
+
     for node in tree.iter_descendants("postorder"):
-        if node.is_leaf():
-            # get the first 2 letter of the node name
-            # print (node.name)
-            name = node.name.split('_')
-            # it is the first 3 index of the index 0 of name
-            short = name[0][:3]
-            # get the color
-            for key in color_dic:
-                if short in key:
-                    color = color_dic[key]
-                    break
-            nstyle = NodeStyle()
-            nstyle["fgcolor"] = color
-            node.set_style(nstyle)
-        else:
+        if not node.is_leaf():
             # create face contain initial set info
             initial = TextFace(node.initial)
             node.add_face(initial, column=0, position = "branch-top")
-            
+        else:
+            if bacteria == 'E.Coli':
+                # get the first 2 letter of the node name
+                # print (node.name)
+                name = node.name.split('_')
+                # it is the first 3 index of the index 0 of name
+                short = name[0][:3]
+                # get the color
+                for key in color_dic:
+                    if short in key:
+                        color = color_dic[key]
+                        break
+                nstyle = NodeStyle()
+                nstyle["fgcolor"] = color
+                nstyle["vt_line_color"]=color
+                nstyle["hz_line_color"]=color
+                node.set_style(nstyle)
     tree.show()
 
 main()
