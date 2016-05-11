b3d5d3e8b537e08c2f1cac08d4ca069877532fda
Huy Nguyen <huyn@iastate.edu>
2016-05-11 13:22:07 -0500

    Update to automate group

--- show_tree.py
+++ show_tree.py
@@ -6,51 +6,54 @@
     End     : 05/08/2016
 '''
 from ete3 import *
+import argparse
 import os
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--Operon","-o", help="Operon file name")
+    parser.add_argument("--Group","-g", help="Color Grouping")
+    args = parser.parse_args()
+    return args
+    
 # color group of genome
 # create a dictionary, sadly this is done manually
-alpha=('Cau','Bra','Sin','Agr','Mes','Bru')
-beta=('Hel','Cam','Wol')
-gamma=('Bor','Ral','Nit','Chr','Nei')
-delta=('Xan','Xyl','She','Pse','Vib','Can','Pas','Hae','Buc','Yer','Sal','Esc')
-# assign color
-color_dic={}
-color_dic[alpha]='cyan'
-color_dic[beta]='black'
-color_dic[gamma]='hotpink'
-color_dic[delta]='brown'
+def parse(file):
+    color_dic={}
+    group= open(file,'r')
+    for line in group.readlines():
+        key= line.split(':')[0]
+        color=line.split(':')[1]
+        value = color.split('\n')[0]
+        color_dic[key]=value
+    return color_dic
 
 
-def main():
-    f= raw_input("Which operon you want to show the tree:")
-    bacteria = raw_input("Which bacteria you want to show the tree (E.Coli or B.Sub):")
-    # print os.getcwd()
-    string = './reconstruction_'+bacteria+ '/'+f
-    tree= Tree(string)
+if __name__ == "__main__":
+    start = time.time()
+    args = get_arguments()
+    tree= Tree(args.Operon)
     # using the color dic to color group
-
+    color_dic = parse(args.Group)
     for node in tree.iter_descendants("postorder"):
         if not node.is_leaf():
             # create face contain initial set info
             initial = TextFace(node.initial)
             node.add_face(initial, column=0, position = "branch-top")
         else:
-            if bacteria == 'E.Coli':
-                # get the first 2 letter of the node name
-                # print (node.name)
-                name = node.name.split('_')
-                # it is the first 3 index of the index 0 of name
-                short = name[0][:3]
-                # get the color
-                for key in color_dic:
-                    if short in key:
-                        color = color_dic[key]
-                        break
-                nstyle = NodeStyle()
-                nstyle["fgcolor"] = color
-                nstyle["vt_line_color"]=color
-                nstyle["hz_line_color"]=color
-                node.set_style(nstyle)
+            name = node.name.split('_')
+            # get accesion number
+            short = name[2]+'_'+name[3]
+            # get the color
+            for key in color_dic:
+                if short in key:
+                    color = color_dic[key]
+                    break
+            nstyle = NodeStyle()
+            nstyle["fgcolor"] = color
+            nstyle["vt_line_color"]=color
+            nstyle["hz_line_color"]=color
+            node.set_style(nstyle)
     tree.show()
 
-main()
+
