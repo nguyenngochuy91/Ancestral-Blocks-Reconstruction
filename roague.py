72db97d1a3b49709d93ae229c7e2e8073bb640ef
Huy Nguyen <huyn@iastate.edu>
2017-09-25 12:28:12 -0500

    add output parameter

--- roague.py
+++ roague.py
@@ -30,7 +30,7 @@ def parser_code():
     parser.add_argument("--reference","-r", help="The ncbi accession number for the reference genome (NC_000913 for E_Coli and NC_000964 for B_Sub)")  
     parser.add_argument("--filter","-f", help="The filter file for creating the tree (E_Coli/phylo_order.txt for E_Coli or B_Sub/phylo_order.txt for B-Sub)")  
     parser.add_argument("--method","-m", help="The method to reconstruc ancestral gene block, we support either global or local")       
-    parser.add_argument("--output","-o", help="Output directory to store the result",default = "result/")                  
+    parser.add_argument("--output","-o", help="Output directory to store the result",default = "result")                  
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -43,8 +43,9 @@ if __name__ == '__main__':
     outdir                     = args.output
     # check if we are going to output results in the current directory
     dirs = genomes_directory.split('/')
+    outdir+='/'
     try:
-        os.mkdir(outdir)
+        os.mkdir(outdir+'/')
     except:
         print ("output directory has already been created")
     if len(dirs)>=3: # means that we have to go to subdirectory
